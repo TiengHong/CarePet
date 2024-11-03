fb460fc2775043021deabd3e5e3bfe7ea0e5e6bb
TiengHong <yshong369@gmail.com>
2024-11-03 22:17:29 +0900

    Indexing err fix

--- germinate-server/static/js/script.js
+++ germinate-server/static/js/script.js
@@ -8,7 +8,7 @@ function filter() {
 
   for (i = 0; i < item.length; i++) {
     name = item[i].getElementsByClassName("name");
-    if (name[0].innerHTML.toUpperCase().indexOf(value) || loc[0].innerHTML.toUpperCase().indexOf(value) > -1) {
+    if (name[0].innerHTML.toUpperCase().indexOf(value) > -1) {
       item[i].style.display = "flex";
     } else {
       item[i].style.display = "none";
